ebedf930a6c6715da391eb1f9dd3d0d2449b995a
nicolas <nicolas@127.0.0.1>
2014-12-15 21:44:19 -0500

    Recreated the interface using Swift and added the necessary bridging header files for compatibility with Objective-C.

---
+++ ios/Classes/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Sample
+//
+//  Created by Nicolas Ménard on 2014-12-15.
+//  Copyright (c) 2014 Nicolas Ménard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var unit = CoreUnit(NSString: "Cake")
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        println(unit.getName())
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
