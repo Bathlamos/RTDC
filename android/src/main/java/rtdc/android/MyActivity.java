f51b4baa8c27b5206dd59b5a516f3ece1e44cd4f
Bathlamos <plegault@live.ca>
2015-02-07 11:29:13 -0500

    Now using Gradle instead of Ant/Maven. The iOS project is broken with j2objc. So is the web project.

--- android/src/main/java/rtdc/android/MyActivity.java
+++ android/src/main/java/rtdc/android/MyActivity.java
@@ -1,7 +1,13 @@
 package rtdc.android;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
+import com.couchbase.lite.Manager;
+import com.couchbase.lite.android.AndroidContext;
+
+import java.io.IOException;
 
 public class MyActivity extends Activity {
     /**
@@ -11,5 +17,19 @@ public class MyActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        Manager manager;
+
+        Context mContext = getApplicationContext();
+        try {
+            /*
+             * In Java the Manager instance and all the objects descending
+             * from it may be used on any thread.
+             */
+            manager = new Manager(new AndroidContext(mContext), Manager.DEFAULT_OPTIONS);
+        } catch (IOException e) {
+            Log.e(TAG, "Cannot create Manager instance", e);
+            return;
+        }
     }
 }
