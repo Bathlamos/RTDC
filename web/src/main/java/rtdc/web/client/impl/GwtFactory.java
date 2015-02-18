35978bf2a4606c0eeac282514cca652280eca96d
Bathlamos <plegault@live.ca>
2015-02-18 15:01:24 -0500

    Validation tools working for GWT

--- web/src/main/java/rtdc/web/client/impl/GwtFactory.java
+++ web/src/main/java/rtdc/web/client/impl/GwtFactory.java
@@ -1,14 +1,17 @@
 package rtdc.web.client.impl;
 
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.validation.client.AbstractGwtValidatorFactory;
 import com.google.gwt.validation.client.GwtValidation;
+import com.google.gwt.validation.client.impl.AbstractGwtValidator;
 import rtdc.core.impl.Factory;
 import rtdc.core.impl.HttpRequest;
 import rtdc.core.model.User;
 
+import javax.validation.Validation;
 import javax.validation.Validator;
 
-public class GwtFactory implements Factory {
+public final class GwtFactory extends AbstractGwtValidatorFactory implements Factory {
 
     /**
      * Validator marker for the Validation Sample project. Only the classes and groups listed
@@ -18,13 +21,18 @@ public class GwtFactory implements Factory {
     public interface GwtValidator extends Validator {}
 
     @Override
+    public AbstractGwtValidator createValidator() {
+        return GWT.create(GwtValidator.class);
+    }
+
+    @Override
     public HttpRequest newHttpRequest(String url, HttpRequest.RequestMethod requestMethod) {
         return new GwtHttpRequest(url, requestMethod);
     }
 
     @Override
     public Validator newValidator() {
-        return GWT.create(GwtValidator.class);
+        return Validation.buildDefaultValidatorFactory().getValidator();
     }
 
 }
