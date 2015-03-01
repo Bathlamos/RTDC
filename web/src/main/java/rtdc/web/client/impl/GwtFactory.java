850cb3b8059e7d2192ab3a0f5b8b92c23a8fb8c2
Nicolas Ménard <nicolasm@live.ca>
2015-02-28 20:33:16 -0500

    Removed validators & its dependencies.

--- web/src/main/java/rtdc/web/client/impl/GwtFactory.java
+++ web/src/main/java/rtdc/web/client/impl/GwtFactory.java
@@ -1,39 +1,14 @@
 package rtdc.web.client.impl;
 
-import com.google.gwt.core.client.GWT;
-import com.google.gwt.validation.client.AbstractGwtValidatorFactory;
-import com.google.gwt.validation.client.GwtValidation;
-import com.google.gwt.validation.client.impl.AbstractGwtValidator;
 import rtdc.core.impl.Dispatcher;
 import rtdc.core.impl.Factory;
 import rtdc.core.impl.HttpRequest;
-import rtdc.core.model.User;
 
-import javax.validation.Validation;
-import javax.validation.Validator;
-
-public final class GwtFactory extends AbstractGwtValidatorFactory implements Factory {
-
-    /**
-     * Validator marker for the Validation Sample project. Only the classes and groups listed
-     * in the {@link GwtValidation} annotation can be validated.
-     */
-    @GwtValidation(User.class)
-    public interface GwtValidator extends Validator {}
-
-    @Override
-    public AbstractGwtValidator createValidator() {
-        return GWT.create(GwtValidator.class);
-    }
+public final class GwtFactory implements Factory {
 
     @Override
     public HttpRequest newHttpRequest(String url, HttpRequest.RequestMethod requestMethod) {
         return new GwtHttpRequest(url, requestMethod);
-    }
-
-    @Override
-    public Validator newValidator() {
-        return Validation.buildDefaultValidatorFactory().getValidator();
     }
 
     @Override
