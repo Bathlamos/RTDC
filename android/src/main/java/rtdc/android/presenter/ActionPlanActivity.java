1b1aef7842a7b6b65f74e1cf3dcfdca632b54ca8
Olivier004 <olivierclermont@hotmail.com>
2015-03-22 23:59:57 -0400

    Action Plan ListView Adapter for editing

--- android/src/main/java/rtdc/android/presenter/ActionPlanActivity.java
+++ android/src/main/java/rtdc/android/presenter/ActionPlanActivity.java
@@ -3,9 +3,6 @@ package rtdc.android.presenter;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
-import android.content.res.Configuration;
-import android.graphics.Color;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -49,74 +46,9 @@ public class ActionPlanActivity extends Activity implements ActionListView {
         // Comment this out when connected to server ------
         addActions(5);
         adapter = new ActionListAdapter();
-       // editAdapter = new EditActionListAdapter();
+        editAdapter = new EditActionListAdapter();
         ((AdapterView)actionListView).setAdapter(adapter);
-        //((AdapterView)actionListView).setAdapter(editAdapter);
         // ------------------------------------------------
-
-        /*
-        TextView unitNameHeader = (TextView) findViewById(R.id.unitNameHeader);
-        unitNameHeader.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Collections.sort(units, new NumberAwareStringComparator());
-                adapter.notifyDataSetChanged();
-            }
-        });
-
-        TextView availableBedsHeader = (TextView) findViewById(R.id.availableBedsHeader);
-        availableBedsHeader.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Collections.sort(units, Unit.availableBedsComparator);
-                adapter.notifyDataSetChanged();
-            }
-        });
-
-        TextView potentialDCHeader = (TextView) findViewById(R.id.potentialDCHeader);
-        potentialDCHeader.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Collections.sort(units, Unit.potentialDcComparator);
-                adapter.notifyDataSetChanged();
-            }
-        });
-
-        TextView DCByDeadlineHeader = (TextView) findViewById(R.id.DCByDeadlineHeader);
-        DCByDeadlineHeader.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Collections.sort(units, Unit.dcByDeadlineComparator);
-                adapter.notifyDataSetChanged();
-            }
-        });
-
-        TextView totalAdmitsHeader = (TextView) findViewById(R.id.totalAdmitsHeader);
-        totalAdmitsHeader.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Collections.sort(units, Unit.totalAdmitsComparator);
-                adapter.notifyDataSetChanged();
-            }
-        });
-
-        TextView admitsByDeadlineHeader = (TextView) findViewById(R.id.admitsByDeadlineHeader);
-        admitsByDeadlineHeader.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Collections.sort(units, Unit.admitsByDeadlineComparator);
-                adapter.notifyDataSetChanged();
-            }
-        });
-
-        TextView statusAtDeadlineHeader = (TextView) findViewById(R.id.statusAtDeadlineHeader);
-        statusAtDeadlineHeader.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Collections.sort(units, Unit.statusAtDeadlineComparator);
-                adapter.notifyDataSetChanged();
-            }
-        });*/
     }
 
     @Override
@@ -140,25 +72,22 @@ public class ActionPlanActivity extends Activity implements ActionListView {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-
-
         // Handle presses on the action bar items
         switch (item.getItemId()) {
             case R.id.editActionPlan:
                 setEditable = true;
-                editAdapter.notifyDataSetChanged();
+                ((AdapterView)actionListView).setAdapter(editAdapter);
                 invalidateOptionsMenu();
                 return true;
             case R.id.saveActionPlan:
-                //updateActionPlan();
                 invalidateOptionsMenu();
                 setEditable = false;
-                adapter.notifyDataSetChanged();
+                ((AdapterView)actionListView).setAdapter(adapter);
                 return true;
             case R.id.discardActionPlan:
                 invalidateOptionsMenu();
                 setEditable = false;
-                adapter.notifyDataSetChanged();
+                ((AdapterView)actionListView).setAdapter(adapter);
                 return true;
             case R.id.action_go_to_manage:
                 Intent intent = new Intent(this, MyActivity.class);
@@ -204,10 +133,9 @@ public class ActionPlanActivity extends Activity implements ActionListView {
         }
     }
 
-    /*
     private class EditActionListAdapter extends ArrayAdapter<Action> {
         public EditActionListAdapter(){
-            super(ActionPlanActivity.this, R.layout.adapter_action_plan, actions);
+            super(ActionPlanActivity.this, R.layout.adapter_edit_action_plan, actions);
         }
 
         @Override
@@ -215,70 +143,36 @@ public class ActionPlanActivity extends Activity implements ActionListView {
             boolean actionListViewIsNull = false;
 
             if(view == null){
-                view = getLayoutInflater().inflate(R.layout.adapter_action_plan, parent, false);
+                view = getLayoutInflater().inflate(R.layout.adapter_edit_action_plan, parent, false);
                 actionListViewIsNull = true;
             }
 
             Action currentAction = actions.get(position);
 
-            TextView status = (TextView) view.findViewById(R.id.status);
-            status.setText(currentAction.getStatus());
+            Spinner status = (Spinner) view.findViewById(R.id.editStatus);
+            ArrayAdapter<CharSequence> statusAdapter = ArrayAdapter.createFromResource(context, R.array.action_status, android.R.layout.simple_spinner_item);
+            statusAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            status.setAdapter(statusAdapter);
 
-            EditText availableBeds = (EditText) view.findViewById(R.id.availableBeds);
-            availableBeds.setText(Integer.toString(currentUnit.getAvailableBeds()));
-            availableBeds.setTag(currentUnit.getName()+":"+1);
+            TextView role = (TextView) view.findViewById(R.id.editRole);
+            role.setText(currentAction.getRole());
 
-            EditText potentialDC = (EditText) view.findViewById(R.id.potentialDC);
-            potentialDC.setText(Integer.toString(currentUnit.getPotentialDc()));
-            potentialDC.setTag(currentUnit.getName()+":"+2);
+            Spinner action = (Spinner) view.findViewById(R.id.editAction);
+            ArrayAdapter<CharSequence> actionAdapter = ArrayAdapter.createFromResource(context, R.array.actions, android.R.layout.simple_spinner_item);
+            actionAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            action.setAdapter(actionAdapter);
 
-            EditText DCByDeadline = (EditText) view.findViewById(R.id.DCByDeadline);
-            DCByDeadline.setText(Integer.toString(currentUnit.getDcByDeadline()));
-            DCByDeadline.setTag(currentUnit.getName()+":"+3);
+            TextView target = (TextView) view.findViewById(R.id.editTarget);
+            target.setText(currentAction.getTarget());
 
-            EditText totalAdmits = (EditText) view.findViewById(R.id.totalAdmits);
-            totalAdmits.setText(Integer.toString(currentUnit.getTotalAdmits()));
-            totalAdmits.setTag(currentUnit.getName()+":"+4);
+            TextView deadline = (TextView) view.findViewById(R.id.editDeadline);
+            deadline.setText(currentAction.getDeadline());
 
-            EditText admitsByDeadline = (EditText) view.findViewById(R.id.admitsByDeadline);
-            admitsByDeadline.setText(Integer.toString(currentUnit.getAdmitsByDeadline()));
-            admitsByDeadline.setTag(currentUnit.getName()+":"+5);
-
-            TextView statusAtDeadline = (TextView) view.findViewById(R.id.statusAtDeadline);
-            statusAtDeadline.setText(Integer.toString(status));
+            EditText notes = (EditText) view.findViewById(R.id.editNotes);
+            notes.setText(currentAction.getNotes());
 
             if(actionListViewIsNull) {
-                availableBeds.addTextChangedListener(new GenericTextWatcher(currentUnit.getName(), "1"));
-                potentialDC.addTextChangedListener(new GenericTextWatcher(currentUnit.getName(), "2"));
-                DCByDeadline.addTextChangedListener(new GenericTextWatcher(currentUnit.getName(), "3"));
-                totalAdmits.addTextChangedListener(new GenericTextWatcher(currentUnit.getName(), "4"));
-                admitsByDeadline.addTextChangedListener(new GenericTextWatcher(currentUnit.getName(), "5"));
-            }
-
-            if(setEditable){
-                Drawable originalBackground = new EditText(this.getContext()).getBackground();
-
-                availableBeds.setEnabled(true);
-                availableBeds.setBackground(originalBackground);
-                potentialDC.setEnabled(true);
-                potentialDC.setBackground(originalBackground);
-                DCByDeadline.setEnabled(true);
-                DCByDeadline.setBackground(originalBackground);
-                totalAdmits.setEnabled(true);
-                totalAdmits.setBackground(originalBackground);
-                admitsByDeadline.setEnabled(true);
-                admitsByDeadline.setBackground(originalBackground);
-            } else {
-                availableBeds.setEnabled(false);
-                availableBeds.setBackgroundColor(Color.TRANSPARENT);
-                potentialDC.setEnabled(false);
-                potentialDC.setBackgroundColor(Color.TRANSPARENT);
-                DCByDeadline.setEnabled(false);
-                DCByDeadline.setBackgroundColor(Color.TRANSPARENT);
-                totalAdmits.setEnabled(false);
-                totalAdmits.setBackgroundColor(Color.TRANSPARENT);
-                admitsByDeadline.setEnabled(false);
-                admitsByDeadline.setBackgroundColor(Color.TRANSPARENT);
+                notes.addTextChangedListener(new GenericTextWatcher(currentAction.getId(), "5"));
             }
 
             return view;
@@ -286,15 +180,11 @@ public class ActionPlanActivity extends Activity implements ActionListView {
 
         private class GenericTextWatcher implements TextWatcher {
 
-            private View view;
-            private String unit;
+            private int actionId;
             private String field;
 
-            private GenericTextWatcher(View view) {
-                this.view = view;
-            }
-            private GenericTextWatcher(String unit, String field) {
-                this.unit = unit;
+            private GenericTextWatcher(int actionId, String field) {
+                this.actionId = actionId;
                 this.field = field;
             }
             @Override
@@ -304,18 +194,16 @@ public class ActionPlanActivity extends Activity implements ActionListView {
 
             @Override
             public void afterTextChanged(Editable editable) {
-                if (setEditable) {
-                    String value = editable.toString();
-                    // Save the value for the given tag
-                    try {
-                        capacityValues.put(unit+":"+field, Integer.parseInt(value));
-                    } catch (NumberFormatException e) {
+                String value = editable.toString();
+                // Save the value for the given tag
+                try {
+                    actionValues.put(actionId+":"+field, Integer.parseInt(value));
+                } catch (NumberFormatException e) {
 
-                    }
                 }
             }
         }
-    }*/
+    }
 
     private void addActions(int x) {
         for(int i = 0; i < x; i++) {
@@ -346,51 +234,4 @@ public class ActionPlanActivity extends Activity implements ActionListView {
     public void displayError(String title, String error) {
         Toast.makeText(this, title + "\nError: " + error, Toast.LENGTH_SHORT).show();
     }
-
-    /*
-    // Update unit capacities with new values
-    private void updateActionPlan() {
-
-        String[] tag;
-        int value;
-
-        for (HashMap.Entry<String, Integer> capacity : capacityValues.entrySet()) {
-            tag = capacity.getKey().split(":");
-
-            Unit unit = null;
-            for (Unit u : units) {
-                //if (u.getId() == Integer.parseInt(tag[0])) {
-                Logger logger = Logger.getLogger("yolo");
-                logger.log(Level.INFO, u.getName()+" == "+tag[0]+"?");
-                if (u.getName().equals(tag[0])) {
-                    unit = u;
-                    logger.log(Level.INFO, "Yes!!");
-                    break;
-                }
-            }
-
-            value = capacity.getValue();
-
-            switch (Integer.parseInt(tag[1])) {
-                case 1:
-                    if(value != unit.getAvailableBeds()) unit.setAvailableBeds(value);
-                    Logger logger = Logger.getLogger("sdf");
-                    logger.log(Level.INFO, "Changed column "+tag[1]+" for unit "+tag[0]+" with value "+value);
-                    break;
-                case 2:
-                    if(value != unit.getPotentialDc()) unit.setPotentialDc(value);
-                    break;
-                case 3:
-                    if(value != unit.getDcByDeadline()) unit.setDcByDeadline(value);
-                    break;
-                case 4:
-                    if(value != unit.getTotalAdmits()) unit.setTotalAdmits(value);
-                    break;
-                case 5:
-                    if(value != unit.getAdmitsByDeadline()) unit.setAdmitsByDeadline(value);
-                    break;
-            }
-        }
-        capacityValues.clear();
-    }*/
 }
