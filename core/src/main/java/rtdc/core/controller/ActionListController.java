1f766faedba2f7c7b0382a6db8a58e068a5ec063
Nicolas <nicolasm@live.ca>
2015-03-29 16:46:30 -0400

    Fixed 'Edit Action' by correctly implementing the controller and calling the dispatcher.

--- core/src/main/java/rtdc/core/controller/ActionListController.java
+++ core/src/main/java/rtdc/core/controller/ActionListController.java
@@ -1,6 +1,6 @@
 package rtdc.core.controller;
 
-import com.google.common.collect.ImmutableSet;
+import rtdc.core.Bootstrapper;
 import rtdc.core.event.Event;
 import rtdc.core.event.FetchActionsEvent;
 import rtdc.core.model.Action;
@@ -11,15 +11,19 @@ import rtdc.core.view.ActionListView;
 
 import java.util.*;
 
-public class ActionListController implements FetchActionsEvent.Handler {
+public class ActionListController extends Controller<ActionListView>implements FetchActionsEvent.Handler {
 
-    private ActionListView view;
     private Set<Action> actions;
 
     public ActionListController(ActionListView view){
-        this.view = view;
+        super(view);
         Event.subscribe(FetchActionsEvent.TYPE, this);
         Service.getActions();
+    }
+
+    @Override
+    String getTitle() {
+        return "Edit Action";
     }
 
     public List<Action> sortActions(Action.Properties property){
@@ -30,7 +34,7 @@ public class ActionListController implements FetchActionsEvent.Handler {
 
     public void editAction(Action action){
         Cache.getInstance().put("action", action);
-
+        Bootstrapper.FACTORY.newDispatcher().goToEditAction(this);
     }
 
     public void deleteAction(Action action) {
