aab3d139ef4858c79a0a62783ac2211a34834e13
Nicolas Ménard <nicolasm@live.ca>
2015-10-07 21:19:48 -0400

    #98 iOS - Solve J2ObjC issues.

--- core/src/main/resources/rtdc/core/super/java/lang/ref/WeakReference.java
+++ core/src/main/resources/rtdc/core/super/java/lang/ref/WeakReference.java
@@ -1,6 +1,9 @@
 package java.lang.ref;
 
+import com.google.j2objc.annotations.J2ObjCIncompatible;
+
 //HACKY class to make Weakreferences compilable under GWT.
+@J2ObjCIncompatible
 public class WeakReference<T extends java.lang.Object> {
 
     private T obj;
