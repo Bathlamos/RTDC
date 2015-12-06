ccc2cd944bdb2b9658495963f43e670bd95d16b8
Jonathan <jerme043@uottawa.ca>
2015-12-06 13:18:17 -0500

    #139 Asterisk info is updated when user is edited

--- web/src/main/java/rtdc/web/server/service/CleanupService.java
+++ web/src/main/java/rtdc/web/server/service/CleanupService.java
@@ -2,6 +2,7 @@ package rtdc.web.server.service;
 
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import rtdc.core.model.Action;
 import rtdc.core.model.Message;
 import rtdc.web.server.config.PersistenceConfig;
 
@@ -18,14 +19,15 @@ import java.util.logging.Logger;
 public class CleanupService implements ServletContextListener {
 
     private ScheduledThreadPoolExecutor executor = new ScheduledThreadPoolExecutor(1);
-    private Future cleanupTask;
+    private Future textMessagesCleanupTask;
+    private Future actionCleanupTask;
 
     @Override
     public void contextInitialized(ServletContextEvent servletContextEvent) {
-        cleanupTask = executor.scheduleWithFixedDelay(new Runnable(){
+        textMessagesCleanupTask = executor.scheduleWithFixedDelay(new Runnable(){
             @Override
             public void run() {
-                Logger.getLogger(CleanupService.class.getName()).log(Level.INFO, "Cleaning task initiated...");
+                Logger.getLogger(CleanupService.class.getName()).log(Level.INFO, "Text message cleaning task initiated...");
 
                 Session session = PersistenceConfig.getSessionFactory().openSession();
                 Transaction transaction = null;
@@ -44,20 +46,51 @@ public class CleanupService implements ServletContextListener {
                     }
                     transaction.commit();
 
-                    Logger.getLogger(CleanupService.class.getName()).log(Level.INFO, "Cleaning task was completed successfully");
+                    Logger.getLogger(CleanupService.class.getName()).log(Level.INFO, "Text message cleaning task was completed successfully");
                 } catch (RuntimeException e) {
                     if(transaction != null)
                         transaction.rollback();
-                    Logger.getLogger(CleanupService.class.getName()).log(Level.SEVERE, "Cleanup task failed!");
+                    Logger.getLogger(CleanupService.class.getName()).log(Level.SEVERE, "Text message cleanup task failed!", e);
                 } finally{
                     session.close();
                 }
             }
         }, 1, 1, TimeUnit.DAYS);
+
+        actionCleanupTask = executor.scheduleWithFixedDelay(new Runnable() {
+            @Override
+            public void run() {
+                Logger.getLogger(CleanupService.class.getName()).log(Level.INFO, "Action cleaning task initiated...");
+
+                Session session = PersistenceConfig.getSessionFactory().openSession();
+                Transaction transaction = null;
+
+                try{
+                    transaction = session.beginTransaction();
+
+                    // Delete all messages that are older than 30 days
+
+                    List<Action> actions = (List<Action>) session.createCriteria(Action.class).list();
+                    for(Action action: actions) {
+
+                    }
+                    transaction.commit();
+
+                    Logger.getLogger(CleanupService.class.getName()).log(Level.INFO, "Action cleaning task was completed successfully");
+                } catch (RuntimeException e) {
+                    if(transaction != null)
+                        transaction.rollback();
+                    Logger.getLogger(CleanupService.class.getName()).log(Level.SEVERE, "Action cleanup task failed!", e);
+                } finally{
+                    session.close();
+                }
+            }
+        }, 12, 12, TimeUnit.HOURS);
     }
 
     @Override
     public void contextDestroyed(ServletContextEvent servletContextEvent) {
-        cleanupTask.cancel(true);
+        textMessagesCleanupTask.cancel(true);
+        actionCleanupTask.cancel(true);
     }
 }
