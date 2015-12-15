6173ac48e7016fb87c3a08e89bfbe18ec8dd0511
Philippe Legault <plegault@live.ca>
2015-12-14 20:27:48 -0500

    MessageFormat now supported

--- core/src/main/resources/rtdc/core/super/rtdc/core/i18n/Bundle.java
+++ core/src/main/resources/rtdc/core/super/rtdc/core/i18n/Bundle.java
@@ -1,7 +1,7 @@
 package rtdc.core.i18n;
 
-import com.google.gwt.i18n.client.Constants;
+import com.google.gwt.i18n.client.Messages;
 
-public interface Bundle extends ResBundleInterface, Constants {
+public interface Bundle extends ResBundleInterface, Messages {
 
 }
